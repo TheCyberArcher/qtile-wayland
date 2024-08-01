fe88adc483f88f14438bed4a959bb81ddafa4608
If cli is a gift from god, using arch makes us prophets † <62696671+TheCyberArcher@users.noreply.github.com>
2024-08-01 23:37:38 +0000

    Update tools.sh

--- tools.sh
+++ tools.sh
@@ -20,7 +20,7 @@ echo "######### qtile configurations files intall #########"
 sudo rm -rf ~/.config/qtile/
 git clone https://github.com/TheCyberArcher/qtile-wayland.git ~/.qtile-dotfiles
 sudo mv ~/.qtile-dotfiles/autostart.sh ~/.config/qtile/
-sudo mv ~/.qtile-dotfiles/autostart.sh ~/.config/qtile/
+sudo mv ~/.qtile-dotfiles/config.py ~/.config/qtile/
 sudo mv ~/.qtile-dotfiles/assets/ ~/.config/qtile/
 
 echo "######### Theme Installation #########"
