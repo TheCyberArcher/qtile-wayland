d193d72704e95a17b0cc13f43a78bebc4346d839
If cli is a gift from god, using arch makes us prophets † <62696671+TheCyberArcher@users.noreply.github.com>
2024-08-01 22:50:12 +0000

    Update autostart.sh

--- qtile/autostart.sh
+++ qtile/autostart.sh
@@ -4,8 +4,4 @@
 wlr-randr --output DP-3 --mode 3440x1440@144 $
 exec lxsession $
 nm-applet $
-signal-desktop --use-tray-icon --start-in-tray --no-sandbox %U &
-flatpak run --branch=stable --arch=x86_64 --command=whatsapp-desktop-linux io.github.mimbrero.WhatsAppDesktop --start-hidden &
-webcord --start-minimized $
 protonmail-bridge --no-window &
-steam -silent &
