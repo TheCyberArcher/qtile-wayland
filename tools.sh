57423dcee07af39be16b5f63b936fb278ba20b24
If cli is a gift from god, using arch makes us prophets † <62696671+TheCyberArcher@users.noreply.github.com>
2024-08-01 22:59:37 +0000

    Add files via upload

---
+++ tools.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+echo "qtile tools for wayland install"
+
+yay -Syu lxsession nwg-look wlr-randr firewalld thunar gvfs tumbler ffmpegthumbnailer flatpak wofi nm-applet gnome-keyring gnome-disk-utility ntfs-3g pavucontrol linux-zen-headers alacrittygrimshot
+
+sleep 3
+
+echo "done ! ... restarting"
