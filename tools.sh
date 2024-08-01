6e71cfc88a3862686c926e35bc1523080c6082fd
If cli is a gift from god, using arch makes us prophets † <62696671+TheCyberArcher@users.noreply.github.com>
2024-08-01 23:28:58 +0000

    Update tools.sh

--- tools.sh
+++ tools.sh
@@ -13,7 +13,7 @@ nmcli con mod "Connexion filaire 1" ipv4.dns "45.90.28.250 45.90.30.250 9.9.9.9 
 
 systemctl enable firewalld
 systemctl start firewalld
-firewall-cmd --set-default-zone=work
+firewall-cmd --set-default-zone=block
 
 echo "######### Theme Installation #########"
 
