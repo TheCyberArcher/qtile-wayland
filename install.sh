d635c46617d5c51ddfe1f59d6b2a506f633ae136
If cli is a gift from god, using arch makes us prophets † <62696671+TheCyberArcher@users.noreply.github.com>
2024-08-03 22:31:32 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 echo "qtile base wayland install"
 
-yay -Syu qtile qtile-extras python-wlroots wlroots python-dbus-next xdg-desktop-portal-wlr xdg-desktop-portal xdg-desktop-portal-gtk xorg-xwayland sddm cava
+yay -Syu qtile qtile-extras python-pywlroots wlroots python-dbus-next xdg-desktop-portal-wlr xdg-desktop-portal xdg-desktop-portal-gtk xorg-xwayland sddm cava
 
 sudo systemctl enable sddm.service
 
