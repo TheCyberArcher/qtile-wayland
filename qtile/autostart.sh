69891889e1014df969771152afe5b14164e6bb39
If cli is a gift from god, using arch makes us prophets † <62696671+TheCyberArcher@users.noreply.github.com>
2025-03-25 23:24:15 +0100

    Update autostart.sh

--- qtile/autostart.sh
+++ qtile/autostart.sh
@@ -1,7 +1,17 @@
 #!/bin/sh
-#Application Startup Script - Qtile 
 
-wlr-randr --output DP-3 --mode 3440x1440@144 $
+# Application Startup
+
+wlr-randr --output DP-1 --mode 3840x2160@238 --scale 1.0 --adaptive-sync disabled &
 exec lxsession $
-nm-applet $
 protonmail-bridge --no-window &
+exec wpaperd -d &
+
+# Discord screensharing patch
+
+export XDG_CURRENT_DESKTOP=qtile &
+export XDG_SESSION_TYPE=wayland &
+dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP=qtile &
+systemctl --user stop pipewire wireplumber xdg-desktop-portal xdg-desktop-portal-wlr &
+sleep 1
+systemctl --user start wireplumber pipewire &
