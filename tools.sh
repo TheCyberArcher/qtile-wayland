a615518987ba66d144d013400a848ce22ad249fa
If cli is a gift from god, using arch makes us prophets † <62696671+TheCyberArcher@users.noreply.github.com>
2025-03-25 23:15:09 +0100

    Update tools.sh

--- tools.sh
+++ tools.sh
@@ -4,16 +4,7 @@ echo "######### Base tools installation #########"
 
 echo "starting !"
 
-yay -Syu lxsession nwg-look wlr-randr firewalld thunar gvfs tumbler ffmpegthumbnailer flatpak wofi nm-applet gnome-keyring gnome-disk-utility ntfs-3g pavucontrol linux-zen-headers alacrittygrimshot
-
-echo "######### Network configuration #########"
-
-nmcli con mod "Connexion filaire 1" ipv4.ignore-auto-dns yes
-nmcli con mod "Connexion filaire 1" ipv4.dns "45.90.28.250 45.90.30.250 9.9.9.9 149.112.112.112 1.1.1.1"
-
-systemctl enable firewalld
-systemctl start firewalld
-firewall-cmd --set-default-zone=block
+yay -Syu lxsession nwg-look wlr-randr thunar gvfs tumbler ffmpegthumbnailer flatpak wofi gnome-keyring gnome-disk-utility ntfs-3g pavucontrol linux-zen-headers alacritty grimshot
 
 echo "######### qtile configurations files intall #########"
 
